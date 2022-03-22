3aedd5b0fb569f9f5e3873e3e32baf2401b5a08c
Gopalsamy, Nithyanantham <Nithyanantham.Gopalsamy@legato.com>
2022-03-22 19:52:41 +0530

    Updating references of introduction

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,7 +1,7 @@
 import "./home.css";
 import Contact from "../contact/contact";
 import Experience from "../experience/experience";
-import Introduction from "../intro/introduction-component";
+import Introduction from "../intro/introduction";
 import Skills from "../skills/skills";
 import { useRef } from "react";
 import Blogs from "../blog/blog";
